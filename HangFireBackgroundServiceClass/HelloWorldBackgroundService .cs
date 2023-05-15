8d48144a0d64994a5ee8c419aa39b696d5b1aa25
WeamHaleemi <weamhaleemi@gmail.com>
2023-05-15 22:19:26 +0300

    Initial commit

---
+++ HangFireBackgroundServiceClass/HelloWorldBackgroundService .cs
@@ -0,0 +1,43 @@
+﻿
+using Hangfire;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace HangFireBackgroundServiceClass
+{
+    public class HelloWorldBackgroundService : BackgroundService
+    {
+        private readonly IRecurringJobManager _recurringJobManager;
+
+        public HelloWorldBackgroundService(IRecurringJobManager recurringJobManager)
+        {
+            _recurringJobManager = recurringJobManager;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _recurringJobManager.AddOrUpdate(
+                "HelloWorldJob",
+                () => Console.WriteLine("Hello, World!"),
+                "*/1 * * * *" // Run every 5 seconds
+            );
+
+            return Task.CompletedTask;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // Your background service logic (if any)
+            return Task.CompletedTask;
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _recurringJobManager.RemoveIfExists("HelloWorldJob");
+
+            return Task.CompletedTask;
+        }
+    }
+
+}
---
+++ HangFireBackgroundServiceClass/Program.cs
@@ -0,0 +1,39 @@
+using Hangfire;
+using HangFireBackgroundServiceClass;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHangfire(configuration => configuration
+        .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection")));
+
+// Add the processing server as IHostedService
+builder.Services.AddHangfireServer();
+builder.Services.AddHostedService<HelloWorldBackgroundService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseHangfireDashboard();
+
+app.Run();
